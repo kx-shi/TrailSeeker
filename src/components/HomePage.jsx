1862b1a75d492ec25ef9e074b4a278392e18774d
ouozturk <ozan.ugur.ozturk@student.forsbergsskola.se>
2023-11-28 18:38:27 +0100

    50 trail is fetched succesfully
    - We need to figure out how to create a dataset with these specific IDs.

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react';
 import TrailContainer from './TrailContainer';
 
 function HomePage({ userLocation }) {
-  const [trails, setTrails] = useState([]);
+  const [trails, setTrails] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     // URL of your API request
-    const apiUrl = `https://www.outdooractive.com/api/project/api-dev-oa/nearby/tour?location=${userLocation}&sortby=distance&radius=5000&limit=50&key=yourtest-outdoora-ctiveapi`;
+    const apiUrl = `https://www.outdooractive.com/api/project/api-dev-oa/nearby/tour?location=${userLocation}&sortby=distance&radius=500000&limit=50`;
 
     // Set the headers to accept JSON
     const headers = new Headers({
@@ -19,22 +21,52 @@ function HomePage({ userLocation }) {
       method: 'GET',
       headers: headers
     })
-      .then(response => response.json())
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
       .then(data => {
-        // Handle the JSON response here
-        setTrails(data.results || []);
-        console.log(data);
+        // Check if the response status is 'OK'
+        if (data.status === 'OK') {
+          // Handle the JSON response here
+          if (data.result && data.result.length > 0) {
+            setTrails(data.result);
+          } else {
+            // No trails found
+            setTrails(null);
+            setError('No trails found for the specified location. Try increasing the search radius.');
+          }
+          console.log('Data:', data.result);  // Log the data set
+        } else if (data.status === 'ZERO_RESULTS') {
+          // Handle case where no results were found
+          setTrails(null);
+          setError('No trails found for the specified location. Try increasing the search radius.');
+        } else {
+          console.error('Error:', data);
+          setError('Error fetching data');
+        }
       })
       .catch(error => {
         // Handle errors here
         console.error('Error:', error);
+        setError('Error fetching data');
+      })
+      .finally(() => {
+        // Set loading to false when request is complete
+        setIsLoading(false);
       });
   }, [userLocation]);
 
   return (
     <div>
-      {/* Pass the trails data to TrailContainer component */}
-      <TrailContainer trails={trails} />
+      {isLoading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {!isLoading && trails === null && !error && <p>No trails available. Try increasing the search radius.</p>}
+      {!isLoading && trails !== null && (
+        <TrailContainer trails={trails} />
+      )}
     </div>
   );
 }
