bb197e08920f7be3d8543ae2697b75bf1e431ca3
Emelie Segell <segell.emelie@gmail.com>
2023-12-01 21:23:04 +0100

    empty trail page component added and linked to

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,17 @@
-import { NavLink, useLocation,useNavigate  } from "react-router-dom";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
 
 export const NavBar = () => {
-    const location = useLocation();
-    const navigate = useNavigate();
+  const location = useLocation();
+  const navigate = useNavigate();
   return (
     <nav>
-    
       <NavLink to="/">Home</NavLink>
       <NavLink to="/trails">All Trails</NavLink>
       <NavLink to="/trails?filter=favorites">Favorite Trails</NavLink>
-    
-  </nav>
+      <NavLink to="/trails-page">TrailPage</NavLink>
+    </nav>
   );
 };
 
-
 // The names are just examples
+//Link to trailpage added for test purposes
---
+++ src/components/TrailPage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const TrailPage = () => {
+  return <div>TrailPage</div>;
+};
