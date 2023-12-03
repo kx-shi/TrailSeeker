0a303dbd0085c8a403eb56855d96042e2cda42f5
Karolina Shi <karolina@Karolinas-MacBook-Pro.local>
2023-12-03 20:10:20 +0100

    Cherry-pick from trailcontainer branch and add styling on top of it

--- src/components/TrailCard.jsx
+++ src/components/TrailCard.jsx
@@ -2,19 +2,16 @@ import React from 'react';
 import { Route, BrowserRouter, Routes, Link } from "react-router-dom";
 import "../styles/TrailCard.css";
 
-
-
-
 export const TrailCard = ({ trailObject }) => {
 
   return (
     <div className="trail-card">
-    <Link key={trailObject.id} to={"/"}>
-    <img src={`https://img.oastatic.com/img/${trailObject.primaryImage.id}/.jpg`}></img> 
+      <Link key={trailObject.id} to={"/"}>
+        <img src={`https://img.oastatic.com/img/${trailObject.primaryImage.id}/.jpg`} alt={trailObject.title}></img> 
         <h2>{trailObject.title}</h2> 
-    </Link>
-        <p>Duration: {Math.floor(trailObject.time.min / 60)} h {trailObject.time.min % 60} min</p>
-        <img className="activity-category" src={trailObject.category.iconUrl} alt={trailObject.category.name}></img> 
+      </Link>
+      <p>Duration: {Math.floor(trailObject.time.min / 60)} h {trailObject.time.min % 60} min</p>
+      <img className="activity-category" src={trailObject.category.iconUrl} alt={trailObject.category.name} title={trailObject.category.name}></img> 
      </div>
   );
 };
