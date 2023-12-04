9012dc8466e461fd6da9144dbcdb3c524b6e5a8c
Carolina-Cardenas <carolina.beatriz.cardenas@gmail.com>
2023-12-04 21:10:23 +0100

    separated filter from navbar.

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import {TrailContainer} from './TrailContainer';
 import { NavBar } from './NavBar';
+import { Filter } from './Filter';
 import '../styles/HomePage.css';
 
 export const HomePage = ({ userLocation }) => {
@@ -114,6 +115,12 @@ export const HomePage = ({ userLocation }) => {
     return (
         <div className='homepage-container'>
             <NavBar  
+                trailObjects={backupTrailObjects} 
+                setTrailObjects={setTrailObjects} 
+                backupTrailObjects={backupTrailObjects}
+                setBackupTrailObjects={setBackupTrailObjects}
+                />
+                <Filter
                 trailObjects={backupTrailObjects} 
                 setTrailObjects={setTrailObjects} 
                 backupTrailObjects={backupTrailObjects}
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,98 +1,22 @@
-import { useState } from "react";
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import "../styles/NavBar.css";
 
 export const NavBar = ({
-  trailObjects,
-  setTrailObjects,
-  backupTrailObjects,
-  setBackupTrailObjects,
+  
 }) => {
-  const [difficultyFilter, setDifficultyFilter] = useState([]);
-  const [ratingFilter, setRatingFilter] = useState([]);
-  const [rankingFilter, setRankingFilter] = useState([]);
-
   const location = useLocation();
   const navigate = useNavigate();
-  let filteredTrails = {};
+ 
 
-  const handleFilter = () => {
-    setBackupTrailObjects([...trailObjects]);
-    if (difficultyFilter.length > 0) {
-      const difficulties = trailObjects.filter((trail) => trail.difficulties);
-      filteredTrails = difficulties.filter(
-        (d) => d.difficulties.difficulty[0].value === difficultyFilter
-      );
-    }
-
-    if (ratingFilter.length > 0) {
-      console.log(typeof trailObjects[0].rating.qualityOfExperience);
-      console.log(typeof ratingFilter);
-      filteredTrails = trailObjects.filter(
-        (trail) => trail.rating.qualityOfExperience.toString() === ratingFilter
-      );
-    }
-    console.log("filtereed", filteredTrails);
-    setTrailObjects(filteredTrails);
-  };
-  const handleRestoreOriginal = () => {
-    if (backupTrailObjects) {
-      setTrailObjects([...backupTrailObjects]);
-      setBackupTrailObjects(null);
-    }
-  };
   const goToHomePage = () => {
     navigate("/", { state: { userLocation: "18.0649,59.3293" } });
   };
 
-  const handleApplyFilter = () => {
-    handleRestoreOriginal();
-    handleFilter();
-  };
-
   return (
     <div>
-      <select
-        value={difficultyFilter || []}
-        onChange={(e) => {
-          setRatingFilter("");
-          setDifficultyFilter(e.target.value === "" ? null : e.target.value);
-        }}
-      >
-        <option value="">All Difficulties</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-      </select>
-      <select
-        value={ratingFilter || []}
-        onChange={(e) => {
-          setDifficultyFilter("");
-          setRatingFilter(e.target.value === "" ? null : e.target.value);
-        }}
-      >
-        <option value="">All Ratings</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-      </select>
-      <input
-        type="number"
-        placeholder="Enter Ranking"
-        value={rankingFilter || ""}
-        onChange={(e) =>
-          setRankingFilter(
-            e.target.value === "" ? null : parseInt(e.target.value)
-          )
-        }
-      />
-      <button onClick={handleApplyFilter}>Apply Filters</button>
-      <button onClick={goToHomePage}>Go to Home</button>
+     <NavLink to="/" onClick={goToHomePage} activeClassName="active-link">
+        Go to Home
+      </NavLink>
       <NavLink to="/trails-page">TrailPage</NavLink>
     </div>
   );
