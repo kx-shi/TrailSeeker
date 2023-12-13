420f55813c94a9865634c7a584684fd5b8efb298
ouozturk <ozan.ugur.ozturk@student.forsbergsskola.se>
2023-12-13 18:26:34 +0100

    Tips are also included with better way for that InnerHTML problem

--- src/components/TrailDetailsPart/TrailDescriptionInfo.jsx
+++ src/components/TrailDetailsPart/TrailDescriptionInfo.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 
 const TrailDescriptionInfo = ({ gettingThere, tips }) => {
-  const hasHtmlTags = /<\/?[a-z][\s\S]*>/i.test(gettingThere);
-
   return (
     <div className="trail-description-info">
       <h3>Getting There</h3>
-      {hasHtmlTags ? (
-        <div dangerouslySetInnerHTML={{ __html: gettingThere }} />
+      {gettingThere ? (
+        <p dangerouslySetInnerHTML={{ __html: gettingThere }} />
       ) : (
-        <p>{(gettingThere !== undefined) ? gettingThere : "No information available"}</p>
+        <p>No information available</p>
       )}
       <h3>Tips for the Tour</h3>
-      <p>{(tips !== undefined) ? tips : "No tips available"}</p>
+      {tips ? (
+        <p dangerouslySetInnerHTML={{ __html: tips }} />
+      ) : (
+        <p>No tips available</p>
+      )}
     </div>
   );
 };
