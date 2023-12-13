f4be77a54d77474992d23131c96e7773cf7c29e0
ouozturk <ozan.ugur.ozturk@student.forsbergsskola.se>
2023-12-13 18:20:46 +0100

    Getting There HTML rag usage is fixed

--- src/components/TrailDetailsPart/TrailDescriptionInfo.jsx
+++ src/components/TrailDetailsPart/TrailDescriptionInfo.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
 const TrailDescriptionInfo = ({ gettingThere, tips }) => {
-  console.log("tips", gettingThere)
+  const hasHtmlTags = /<\/?[a-z][\s\S]*>/i.test(gettingThere);
+
   return (
     <div className="trail-description-info">
       <h3>Getting There</h3>
-      <p>{(gettingThere !== undefined) ? gettingThere : "No information available"}</p>
+      {hasHtmlTags ? (
+        <div dangerouslySetInnerHTML={{ __html: gettingThere }} />
+      ) : (
+        <p>{(gettingThere !== undefined) ? gettingThere : "No information available"}</p>
+      )}
       <h3>Tips for the Tour</h3>
       <p>{(tips !== undefined) ? tips : "No tips available"}</p>
     </div>
