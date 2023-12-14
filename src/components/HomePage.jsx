5b9201bb925e4609f00034c6f578647514e7db67
ouozturk <ozan.ugur.ozturk@student.forsbergsskola.se>
2023-12-14 19:17:53 +0100

    Eslint used fixes v2

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -8,80 +8,10 @@ import "../styles/HomePage.css";
 
 export const HomePage = ({ userLocation, savedTrails }) => {
   const [backupTrailObjects, setBackupTrailObjects] = useState(null);
-  const [trails, setTrails] = useState(null); // State for storing trail data from our first API request for a specific location
-  const [trailObjects, setTrailObjects] = useState([]); // State for storing detailed trail objects, we will pass these to container to use them inside there
-  const [isLoading, setIsLoading] = useState(true); // State for loading status (we can use it later for better user experience)
+  const [trails, setTrails] = useState(null);
+  const [trailObjects, setTrailObjects] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
-
-  // Effect to fetch trail data when user location changes (we might change this to another style)
-  useEffect(() => {
-    // API URL for fetching nearby trails
-    // I used hardcoded radius, we can discuss about it
-    const apiUrl = `https://www.outdooractive.com/api/project/api-dev-oa/nearby/tour?location=${userLocation}&sortby=distance&radius=500000&limit=15`;
-    // Headers for the API request to get JSON body response
-    const headers = new Headers({
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    });
-
-    fetch(apiUrl, {
-      method: "GET",
-      headers: headers,
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        if (data.status === "OK") {
-          if (data.result && data.result.length > 0) {
-            setTrails(data.result);
-            // Fetching the trail objects here with using ID's
-            fetchTrailObjects(data.result);
-          } else {
-            setTrails(null);
-            setError(
-              "No trails found for the specified location. Try increasing the search radius."
-            );
-          }
-        } else if (data.status === "ZERO_RESULTS") {
-          setTrails(null);
-          setError(
-            "No trails found for the specified location. Try increasing the search radius."
-          );
-        } else {
-          console.error("Error:", data);
-          setError("Error fetching data");
-        }
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-        setError("Error fetching data");
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, [userLocation]);
-
-  // Function to fetch detailed trail objects
-  const fetchTrailObjects = async (trailData) => {
-    try {
-      const trailObjectArray = [];
-      // Looping through each trail ID and fetch detailed data for that ID
-      for (const trail of trailData) {
-        const trailId = trail.id;
-        const trailObject = await fetchTrailData(trailId);
-        trailObjectArray.push(trailObject);
-      }
-      setTrailObjects(trailObjectArray);
-      setBackupTrailObjects([...trailObjectArray]);
-      setTimeout(() => {}, 700); // to get one log for not to confuse
-    } catch (error) {
-      console.error("Error fetching trail objects:", error);
-    }
-  };
 
   // Function to fetch detailed trail data
   const fetchTrailData = async (trailId) => {
@@ -113,7 +43,73 @@ export const HomePage = ({ userLocation, savedTrails }) => {
       return null;
     }
   };
-  // Rendering zone might require some update
+
+  // Effect to fetch trail data when user location changes
+  useEffect(() => {
+    // Function to fetch detailed trail objects
+    const fetchTrailObjects = async (trailData) => {
+      try {
+        const trailObjectArray = [];
+        for (const trail of trailData) {
+          const trailId = trail.id;
+          const trailObject = await fetchTrailData(trailId);
+          trailObjectArray.push(trailObject);
+        }
+        setTrailObjects(trailObjectArray);
+        setBackupTrailObjects([...trailObjectArray]);
+        setTimeout(() => {}, 700); // to get one log for not to confuse
+      } catch (error) {
+        console.error("Error fetching trail objects:", error);
+      }
+    };
+
+    const fetchData = async () => {
+      const apiUrl = `https://www.outdooractive.com/api/project/api-dev-oa/nearby/tour?location=${userLocation}&sortby=distance&radius=500000&limit=15`;
+      const headers = new Headers({
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      });
+
+      try {
+        const response = await fetch(apiUrl, {
+          method: "GET",
+          headers: headers,
+        });
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json();
+        if (data.status === "OK") {
+          if (data.result && data.result.length > 0) {
+            setTrails(data.result);
+            fetchTrailObjects(data.result);
+          } else {
+            setTrails(null);
+            setError(
+              "No trails found for the specified location. Try increasing the search radius."
+            );
+          }
+        } else if (data.status === "ZERO_RESULTS") {
+          setTrails(null);
+          setError(
+            "No trails found for the specified location. Try increasing the search radius."
+          );
+        } else {
+          console.error("Error:", data);
+          setError("Error fetching data");
+        }
+      } catch (error) {
+        console.error("Error:", error);
+        setError("Error fetching data");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [userLocation]);
 
   return (
     <div className="homepage-container">
@@ -129,22 +125,18 @@ export const HomePage = ({ userLocation, savedTrails }) => {
       {!isLoading && trails === null && !error && (
         <p>No trails available. Try increasing the search radius.</p>
       )}
-      {!isLoading &&
-        trails !== null &&
-        !savedTrails && ( // Render all trail objects
-          <div>
-            <h2 className="trail-search-title">Trails Near Stockholm</h2>
-            <TrailContainer trailObjects={trailObjects} />
-          </div>
-        )}
-      {!isLoading &&
-        trails !== null &&
-        savedTrails && ( // Render only liked trail objects
-          <div>
-            <h2>Liked Trails</h2>
-            <LikedTrailsContainer />
-          </div>
-        )}
+      {!isLoading && trails !== null && !savedTrails && (
+        <div>
+          <h2 className="trail-search-title">Trails Near Stockholm</h2>
+          <TrailContainer trailObjects={trailObjects} />
+        </div>
+      )}
+      {!isLoading && trails !== null && savedTrails && (
+        <div>
+          <h2>Liked Trails</h2>
+          <LikedTrailsContainer />
+        </div>
+      )}
     </div>
   );
 };
