99d334621bd1cefc731e3090f8c51ad8f016ee2c
ouozturk <ozan.ugur.ozturk@student.forsbergsskola.se>
2023-12-14 10:22:00 +0100

    TrailImage is cleaned and simplified

--- src/components/TrailDetailsPart/TrailImages.jsx
+++ src/components/TrailDetailsPart/TrailImages.jsx
@@ -17,36 +17,33 @@ const TrailImages = ({ images }) => {
   const currentImage = images.image[currentIndex];
 
   return (
-    <div className="trail-image-container">
-      <div className="trail-images-unique">
-        <div className="image-container">
-          <div className="photo-box">
-            <img
-              src={`https://img.oastatic.com/img2/${currentImage.id}/default/variant.jpg`}
-              alt={currentImage.title}
-              className="image"
-            />
-          </div>
-          <span className="vignette"></span>
-          <div className="arrow left" onClick={handlePrev}>
-            {"<"}
-          </div>
-          <div className="arrow right" onClick={handleNext}>
-            {">"}
-          </div>
+    <div className="trail-image">
+      <div className="trail-image-container">
+        <div className="trail-image-box">
+          <img
+            src={`https://img.oastatic.com/img2/${currentImage.id}/default/variant.jpg`}
+            alt={currentImage.title}
+            className="image"
+          />
         </div>
-        <div className="image-description">
-          <span className="image-title">
-            <p>{currentImage.title}</p>
-          </span>
-          <div className="information-box">
-            {currentImage.author ? <p>Author: {currentImage.author}</p> : null}
-            {currentImage.source ? <p>Source: {currentImage.source}</p> : null}
-          </div>
+        <span className="vignette"></span>
+        <div className="arrow left" onClick={handlePrev}>
+          {"<"}
+        </div>
+        <div className="arrow right" onClick={handleNext}>
+          {">"}
+        </div>
+      </div>
+      <div className="image-description">
+        <span className="image-title">
+          <p>{currentImage.title}</p>
+        </span>
+        <div className="information-box">
+          {currentImage.author ? <p>Author: {currentImage.author}</p> : null}
+          {currentImage.source ? <p>Source: {currentImage.source}</p> : null}
         </div>
       </div>
     </div>
-
   );
 };
 
