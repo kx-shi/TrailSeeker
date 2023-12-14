6d4d9c4ffc8192a8bafb26ea0ca1ce0e2c5cf602
karo <karolina.shi@outlook.com>
2023-12-14 20:37:27 +0100

    Add padding to app container, remove horizontal scrolling

--- src/components/TrailPage.jsx
+++ src/components/TrailPage.jsx
@@ -2,11 +2,12 @@ import { CommentContainer } from "./CommentContainer";
 import { NavBar } from "./NavBar";
 import { useParams } from "react-router-dom";
 import TrailDetails from "./TrailDetails";
+import '../styles/TrailPage.css'
 
 export const TrailPage = () => {
   const { trailId } = useParams();
   return (
-    <div>
+    <div className="trail-page-container">
       <NavBar />
       <TrailDetails trailId={trailId} />
       <CommentContainer trailID={trailId} />
